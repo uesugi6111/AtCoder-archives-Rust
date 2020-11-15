567a91486abb57facce543bfa534ffbe0326dac7
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2020-11-15 11:50:17 +0000

    agc049

---
+++ agc049/src/bin/a.rs
@@ -0,0 +1,11 @@
+#[allow(unused_imports)]
+use proconio::{
+    fastout, input,
+    marker::{Bytes, Chars, Isize1, Usize1},
+};
+#[fastout]
+fn main() {
+    input!(n: usize, s: [Chars; n]);
+
+    println!("{}", n);
+}
