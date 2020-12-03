41c8eb118c016f5c31a6f3c3fb584b2fe2a7f271
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2020-12-03 16:12:21 +0000

    EDPC D

--- dp/src/bin/d.rs
+++ dp/src/bin/d.rs
@@ -5,6 +5,18 @@ use proconio::{
 };
 #[fastout]
 fn main() {
-    input!(n: usize, w: usize, wv: [(i64, i64); n]);
-    println!("{}", n);
+    input!(n: usize, w: usize, wv: [(usize, i64); n]);
+
+    let mut dp = vec![vec![0i64; w + 1]; n + 1];
+
+    for i in 0..n {
+        for j in 0..=w {
+            if wv[i].0 <= j {
+                dp[i + 1][j] = std::cmp::max(dp[i + 1][j], dp[i][j - wv[i].0] + wv[i].1);
+            }
+            dp[i + 1][j] = std::cmp::max(dp[i + 1][j], dp[i][j]);
+        }
+    }
+
+    println!("{}", dp[n][w]);
 }
