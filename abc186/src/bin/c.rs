c7e475b73befd60bcc33bdedf1ef734d0713047f
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2020-12-24 10:40:23 +0000

    abc186 c bit

--- abc186/src/bin/c.rs
+++ abc186/src/bin/c.rs
@@ -14,9 +14,15 @@ fn main() {
             continue;
         }
 
-        let str8 = format!("{:0o}", i);
-
-        if str8.contains('7') {
+        let mut is_nana = false;
+        for j in 0..10 as u32 {
+            let bit = 7 << (j * 3);
+            if i & bit == bit {
+                is_nana = true;
+                break;
+            }
+        }
+        if is_nana {
             continue;
         }
 
