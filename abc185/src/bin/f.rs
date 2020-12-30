b37995af8510f019266e6408c93c6bdeb9f470f9
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2020-12-30 17:50:09 +0000

    update

--- abc185/src/bin/f.rs
+++ abc185/src/bin/f.rs
@@ -1,10 +1,5 @@
 use fenwicktree::Sum;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Bytes, Chars, Isize1, Usize1},
-};
-#[fastout]
+#[proconio::fastout]
 fn main() {
     input!(n: usize, q: usize, a: [i64; n], txy: [(i32, i64, i64); q]);
 
@@ -21,6 +16,14 @@ fn main() {
             println!("{}", ft.sum((x, y)));
         }
     }
+}
+
+#[rustfmt::skip]
+mod fast_input {
+    // https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+    #[macro_export]macro_rules! input{($($r:tt)*)=>{let s={use std::io::Read;let mut s=String::new();std::io::stdin().read_to_string(&mut s).unwrap();let input=Box::leak(s.into_boxed_str());input};let mut iter=s.split_ascii_whitespace();input_inner!{iter,$($r)*}};}
+    #[macro_export]macro_rules! input_inner{($iter:expr)=>{};($iter:expr,)=>{};($iter:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($iter,$t);input_inner!{$iter $($r)*}};}
+    #[macro_export]macro_rules! read_value{($iter:expr,($($t:tt),*))=>{($(read_value!($iter,$t)),*)};($iter:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($iter,$t)).collect::<Vec<_>>()};($iter:expr,Chars)=>{read_value!($iter,String).chars().collect::<Vec<char>>()};($iter:expr,Usize1)=>{read_value!($iter,usize)-1};($iter:expr,$t:ty)=>{$iter.next().unwrap().parse::<$t>().expect("Parse error")};}
 }
 
 mod fenwicktree {
