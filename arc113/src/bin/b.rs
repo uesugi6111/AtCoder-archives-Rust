507daba439cdcc09adb9b2b62bbc6adfdb969cfb
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-02-21 21:35:35 +0000

    arc113

---
+++ arc113/src/bin/b.rs
@@ -0,0 +1,57 @@
+#[rustfmt::skip]
+mod fast_input {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=fast_input::Scanner::new(std::io::stdin().lock(),4096);input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner {buf:Vec<u8>,pos: usize,}
+    impl Scanner {
+        pub fn new<R: std::io::Read>(mut reader: R, estimated: usize) -> Self {
+            let mut buf = Vec::with_capacity(estimated);let _=std::io::copy(&mut reader,&mut buf).unwrap();if buf.last()!=Some(&b'\n'){panic!("{}", 0);}
+            Scanner { buf, pos: 0 }
+        }
+        #[inline]
+        pub fn next<T: std::str::FromStr>(&mut self) -> T where T::Err: std::fmt::Debug,{
+            let mut start=None;loop{match(self.buf[self.pos],start.is_some()){(b' ',true)|(b'\n', true)|(b'\r', true)=>break,(_, true)|(b' ', false)|(b'\n',false)|(b'\r', false)=>self.pos+=1,(_, false)=>start=Some(self.pos),}}let target=&self.buf[start.unwrap()..self.pos];
+            unsafe { std::str::from_utf8_unchecked(target) }.parse().unwrap()
+        }
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(a: u64, b: u64, c: u64);
+    println!(
+        "{}",
+        modpow(a as i64, modpow(b as i64, c as i64, 4) + 4, 10)
+    );
+}
+
+pub fn modpow(base: i64, exp: i64, n: i64) -> i64 {
+    let (mut base, mut exp, n) = (base, exp, n);
+
+    assert!(
+        exp >= 0,
+        "negative exponent cannot be used in modular exponentiation"
+    );
+
+    if exp == 0 {
+        return 1;
+    }
+
+    let mut res = 1;
+    base %= n;
+
+    loop {
+        if exp % 2 == 1 {
+            res *= &base;
+            res %= &n;
+        }
+
+        if exp == 1 {
+            return res;
+        }
+
+        exp /= 2;
+        base *= base;
+        base %= n;
+    }
+}
---
+++ arc113/src/bin/c.rs
@@ -0,0 +1,46 @@
+#[rustfmt::skip]
+mod fast_input {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=fast_input::Scanner::new(std::io::stdin().lock(),4096);input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner {buf:Vec<u8>,pos: usize,}
+    impl Scanner {
+        pub fn new<R: std::io::Read>(mut reader: R, estimated: usize) -> Self {
+            let mut buf = Vec::with_capacity(estimated);let _=std::io::copy(&mut reader,&mut buf).unwrap();if buf.last()!=Some(&b'\n'){panic!("{}", 0);}
+            Scanner { buf, pos: 0 }
+        }
+        #[inline]
+        pub fn next<T: std::str::FromStr>(&mut self) -> T where T::Err: std::fmt::Debug,{
+            let mut start=None;loop{match(self.buf[self.pos],start.is_some()){(b' ',true)|(b'\n', true)|(b'\r', true)=>break,(_, true)|(b' ', false)|(b'\n',false)|(b'\r', false)=>self.pos+=1,(_, false)=>start=Some(self.pos),}}let target=&self.buf[start.unwrap()..self.pos];
+            unsafe { std::str::from_utf8_unchecked(target) }.parse().unwrap()
+        }
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(ss: Chars);
+    let mut s = ss;
+    let mut count = 0;
+    let mut c = 'Z';
+    let mut last_index = s.len();
+
+    for i in (0..s.len() - 2).rev() {
+        if s[i] == s[i + 1] && s[i] != s[i + 2] {
+            let cnt = s
+                .iter()
+                .skip(i + 2)
+                .take(last_index - (i + 2))
+                .filter(|x| **x == s[i])
+                .count();
+            if s[i] != c {
+                count += s.len() - 2 - i - cnt;
+                c = s[i];
+            } else {
+                count += last_index - 2 - i - cnt;
+            }
+            s[i + 2] = s[i];
+            last_index = i;
+        }
+    }
+    println!("{}", count);
+}
