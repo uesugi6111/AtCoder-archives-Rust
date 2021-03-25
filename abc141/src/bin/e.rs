669cbe69359e4404489bd42d640faa3205590f91
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-03-25 22:24:00 +0000

    ロリハ改修

--- abc141/src/bin/e.rs
+++ abc141/src/bin/e.rs
@@ -1,19 +1,18 @@
 #[rustfmt::skip]
 mod fast_input {
-    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=fast_input::Scanner::new(std::io::stdin().lock(),4096);input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=fast_input::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
     #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
     #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
-    pub struct Scanner {buf:Vec<u8>,pos: usize,}
+    pub struct Scanner {s:Box<str>,input: std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
     impl Scanner {
-        pub fn new<R: std::io::Read>(mut reader: R, estimated: usize) -> Self {
-            let mut buf = Vec::with_capacity(estimated);let _=std::io::copy(&mut reader,&mut buf).unwrap();if buf.last()!=Some(&b'\n'){panic!("{}", 0);}
-            Scanner { buf, pos: 0 }
+        pub fn new<R: std::io::Read>(mut reader: R) -> Self {
+            let s={let mut s = String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()};
+            let mut sc=Scanner {s,input:"".split_ascii_whitespace().peekable(),};
+            let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};
+            sc.input = s.split_ascii_whitespace().peekable();
+            sc
         }
-        #[inline]
-        pub fn next<T: std::str::FromStr>(&mut self) -> T where T::Err: std::fmt::Debug,{
-            let mut start=None;loop{match(self.buf[self.pos],start.is_some()){(b' ',true)|(b'\n', true)=>break,(_, true)|(b' ', false)|(b'\n',false)=>self.pos+=1,(_, false)=>start=Some(self.pos),}}let target=&self.buf[start.unwrap()..self.pos];
-            unsafe { std::str::from_utf8_unchecked(target) }.parse().unwrap()
-        }
+        #[inline] pub fn next<T: std::str::FromStr>(&mut self) -> T where T::Err: std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
     }
 }
 #[proconio::fastout]
@@ -26,11 +25,12 @@ fn main() {
     while ng - ok > 1 {
         mid = (ng + ok) / 2;
         let mut mached = false;
+
         for i in 0..n {
             if i + mid * 2 > n {
                 break;
             }
-            if rh::rolling_hash(&s, i, mid) {
+            if rh::rolling_hash(&s[i..i + mid], &s[i + mid..s.len()]) {
                 mached = true;
                 break;
             }
@@ -45,28 +45,30 @@ fn main() {
 }
 
 mod rh {
-    pub fn rolling_hash(s: &[char], start: usize, l: usize) -> bool {
-        let b: u128 = 1_000_000_007;
-        let target_start = start + l;
 
-        let pow_b = b.wrapping_pow(l as u32);
+    //const BASE: u128 = 2_305_843_009_213_693_951; // 2^61-1
+    const BASE: u128 = 1000000007;
+    pub fn rolling_hash(s: &[char], t: &[char]) -> bool {
+        let l = s.len();
+
+        let pow_b = BASE.wrapping_pow(l as u32);
 
         let mut target_hash: u128 = 0;
         let mut base_hash: u128 = 0;
         for k in 0..l {
-            base_hash = base_hash.wrapping_mul(b) + s[start + k] as u128;
-            target_hash = target_hash.wrapping_mul(b) + s[target_start + k] as u128;
+            base_hash = base_hash.wrapping_mul(BASE) + unsafe { *s.get_unchecked(k) } as u128;
+            target_hash = target_hash.wrapping_mul(BASE) + unsafe { *t.get_unchecked(k) } as u128;
         }
-
-        for k in 0..s.len() - target_start {
+        if target_hash == base_hash {
+            return true;
+        }
+        for k in 0..t.len() - l {
+            target_hash = target_hash
+                .wrapping_mul(BASE)
+                .wrapping_add(unsafe { *t.get_unchecked(l + k) } as u128)
+                .wrapping_sub((unsafe { *t.get_unchecked(k) } as u128).wrapping_mul(pow_b));
             if target_hash == base_hash {
                 return true;
-            }
-            if target_start + l + k < s.len() {
-                target_hash = target_hash
-                    .wrapping_mul(b)
-                    .wrapping_add(s[target_start + l + k] as u128)
-                    .wrapping_sub((s[target_start + k] as u128).wrapping_mul(pow_b));
             }
         }
         false
