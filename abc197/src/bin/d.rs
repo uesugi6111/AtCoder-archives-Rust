42b8b31550a99e18bf16e71e20eb26f3cd267623
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-03-27 14:59:44 +0000

    abc197

---
+++ abc197/src/bin/d.rs
@@ -0,0 +1,36 @@
+#[rustfmt::skip]
+mod fast_input {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=fast_input::Scanner::new(std::io::stdin().lock(),4096);input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner {buf:Vec<u8>,pos: usize,}
+    impl Scanner {
+        pub fn new<R: std::io::Read>(mut reader: R, estimated: usize) -> Self {
+            let mut buf = Vec::with_capacity(estimated);let _=std::io::copy(&mut reader,&mut buf).unwrap();if buf.last()!=Some(&b'\n'){panic!("{}", 0);}
+            Scanner { buf, pos: 0 }
+        }
+        #[inline]
+        pub fn next<T: std::str::FromStr>(&mut self) -> T where T::Err: std::fmt::Debug,{
+            let mut start=None;loop{match(self.buf[self.pos],start.is_some()){(b' ',true)|(b'\n', true)|(b'\r', true)=>break,(_, true)|(b' ', false)|(b'\n',false)|(b'\r', false)=>self.pos+=1,(_, false)=>start=Some(self.pos),}}let target=&self.buf[start.unwrap()..self.pos];
+            unsafe { std::str::from_utf8_unchecked(target) }.parse().unwrap()
+        }
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(n: usize, xy: (f64, f64), xy2: (f64, f64));
+
+    let o = ((xy.0 + xy2.0) / 2.0, (xy.1 + xy2.1) / 2.0);
+
+    let base = (xy.0 - o.0, xy.1 - o.1);
+    let d = (180 * (n - 2)) as f64 / n as f64 / 180.0;
+
+    let sin_d = (d as f64).sin();
+    let cos_d = (d as f64).cos();
+    let v = ((xy.0 - o.0), (xy.1 - o.1));
+
+    let kaiten = ((v.0 * cos_d - v.1 * sin_d), (v.0 * sin_d + v.1 * cos_d));
+    let ans = (kaiten.0 + o.0, kaiten.1 + o.1);
+
+    println!("{} {}", ans.0, ans.1);
+}
