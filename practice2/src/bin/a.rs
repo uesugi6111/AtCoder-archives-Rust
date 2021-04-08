0348f5e1f994736703f7f3aad181897ea0931d97
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-04-08 21:50:28 +0000

    verify dsu

--- practice2/src/bin/a.rs
+++ practice2/src/bin/a.rs
@@ -1,17 +1,101 @@
-use itertools_num::ItertoolsNum;
-#[allow(unused_imports)]
-use proconio::{
-    fastout, input,
-    marker::{Bytes, Chars, Isize1, Usize1},
-};
-#[fastout]
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
 fn main() {
-    input!(n: usize, k: usize, a: [i64; n]);
+    input!(n: usize, q: usize, tuv: [(usize, usize, usize); q]);
 
-    let ruiseki_wa = a.iter().cumsum().collect::<Vec<i64>>();
+    let mut dsu = dsu::Dsu::new(n);
+    for (t, u, v) in tuv {
+        if t == 0 {
+            dsu.unite(u, v);
+        } else {
+            println!("{}", if dsu.is_same(u, v) { 1 } else { 0 });
+        }
+    }
+}
 
-    let mut sum: i64 = (0..n - k).map(|x| ruiseki_wa[x + k] - ruiseki_wa[x]).sum();
-    sum += ruiseki_wa[k - 1];
+mod dsu {
+    use std::collections::{HashMap, HashSet};
+    #[derive(Debug, Clone)]
+    enum Node {
+        Root(usize),
+        Child(usize),
+    }
+    ///UnionFind
+    #[derive(Clone, Debug)]
+    pub struct Dsu {
+        uf: Vec<Node>,
+    }
 
-    println!("{}", sum);
+    impl Dsu {
+        pub fn new(n: usize) -> Dsu {
+            Dsu {
+                uf: vec![Node::Root(1); n],
+            }
+        }
+
+        pub fn root(&mut self, target: usize) -> usize {
+            match *unsafe { self.uf.get_unchecked(target) } {
+                Node::Root(_) => target,
+                Node::Child(par) => {
+                    let root = self.root(par);
+                    self.uf[target] = Node::Child(self.root(par));
+                    root
+                }
+            }
+        }
+        pub fn unite(&mut self, x: usize, y: usize) -> bool {
+            let rx = self.root(x);
+            let ry = self.root(y);
+            if rx == ry {
+                return false;
+            }
+            let size_x = self.size(x);
+            let size_y = self.size(y);
+
+            let (i, j) = if size_x > size_y { (rx, ry) } else { (ry, rx) };
+            self.uf[i] = Node::Root(size_x + size_y);
+            self.uf[j] = Node::Child(i);
+
+            true
+        }
+        pub fn is_same(&mut self, x: usize, y: usize) -> bool {
+            self.root(x) == self.root(y)
+        }
+        pub fn size(&mut self, x: usize) -> usize {
+            let root = self.root(x);
+            match self.uf[root] {
+                Node::Root(size) => size,
+                Node::Child(_) => 0,
+            }
+        }
+        pub fn get_same_group(&mut self, x: usize) -> HashSet<usize> {
+            let root = self.root(x);
+            let mut g = HashSet::new();
+            for i in 0..self.uf.len() {
+                if root == self.root(i) {
+                    g.insert(i);
+                }
+            }
+            g
+        }
+        pub fn get_all_groups(&mut self) -> HashMap<usize, HashSet<usize>> {
+            let mut map: HashMap<usize, HashSet<usize>> = HashMap::new();
+            for i in 0..self.uf.len() {
+                let root = self.root(i);
+
+                map.entry(root).or_insert_with(HashSet::new).insert(i);
+            }
+            map
+        }
+    }
 }
