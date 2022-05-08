7a369a6e5720168db620179a9412a30bbfe735a8
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2022-05-08 13:38:08 +0000

    abc250

---
+++ abc250/src/bin/e.rs
@@ -0,0 +1,49 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(
+        n: usize,
+        a: [i64; n],
+        b: [i64; n],
+        q: usize,
+        xy: [(i64, i64); q]
+    );
+    let mut z = xy
+        .iter()
+        .enumerate()
+        .map(|(i, x)| (x.0, x.1, i))
+        .collect::<Vec<_>>();
+    z.sort_by_key(|x| x.0);
+
+    let mut a_set = std::collections::HashSet::new();
+
+    let mut index = 0;
+
+    let mut ans = vec![false; q];
+    for &(x, y, z) in z.iter() {
+        while (index as i64) < x {
+            a_set.insert(a[index]);
+            index += 1;
+        }
+        let buff = b
+            .iter()
+            .cloned()
+            .take(y as usize)
+            .collect::<std::collections::HashSet<_>>();
+        ans[z] = buff == a_set;
+    }
+
+    for i in ans {
+        println!("{}", if i { "Yes" } else { "No" });
+    }
+}
