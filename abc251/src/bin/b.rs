1b9d2ef3aca71da710393e5c2854ab7e2e0c79d9
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2022-05-14 13:39:42 +0000

    abc251

---
+++ abc251/src/bin/b.rs
@@ -0,0 +1,45 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(n: usize, w: usize, a: [usize; n]);
+
+    let mut ww = vec![false; w + 1];
+
+    for &i in a.iter() {
+        if i > w {
+            break;
+        }
+        ww[i as usize] = true;
+    }
+
+    for i in 0..n {
+        for j in i + 1..n {
+            if a[i] + a[j] > w {
+                continue;
+            }
+            ww[a[i] + a[j]] = true;
+        }
+    }
+
+    for i in 0..n {
+        for j in i + 1..n {
+            for k in j + 1..n {
+                if a[i] + a[j] + a[k] > w {
+                    continue;
+                }
+                ww[a[i] + a[j] + a[k]] = true;
+            }
+        }
+    }
+    println!("{}", ww.iter().filter(|&&x| x).count());
+}
---
+++ abc251/src/bin/c.rs
@@ -0,0 +1,40 @@
+use std::cmp::Ordering;
+
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(n: usize, st: [(String, i64); n]);
+
+    let mut set = std::collections::HashSet::new();
+
+    let mut sti = st
+        .iter()
+        .enumerate()
+        .map(|(i, (s, t))| (s.clone(), *t, i))
+        .collect::<Vec<_>>();
+
+    let mut ans = vec![];
+
+    for i in 0..sti.len() {
+        if !set.contains(&sti[i].0) {
+            set.insert(sti[i].0.clone());
+            ans.push(sti[i].clone());
+        }
+    }
+
+    ans.sort_by(|a, b| match a.1.cmp(&b.1).reverse() {
+        Ordering::Equal => a.2.cmp(&b.2),
+        other => other,
+    });
+    println!("{}", ans[0].2 + 1);
+}
