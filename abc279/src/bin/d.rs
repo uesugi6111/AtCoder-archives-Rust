14fb8989e6af270aa333164e974368cc1fe69660
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2022-11-26 13:36:51 +0000

    abc279

---
+++ abc279/src/bin/d.rs
@@ -0,0 +1,39 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(a: f64, b: f64);
+
+    let mut low = 0;
+    let mut high = 1000000000000000000;
+
+    let mut cnt = 1000;
+    while cnt > 0 {
+        let c1 = (low * 2 + high) / 3;
+        let c2 = (low + high * 2) / 3;
+
+        if f(c1, a, b) > f(c2, a, b) {
+            low = c1;
+        } else {
+            high = c2;
+        }
+        cnt -= 1;
+    }
+    println!(
+        "{}",
+        f(low, a, b).min(f(high, a, b).min(f((high + low) / 2, a, b)))
+    );
+}
+
+fn f(n: i64, a: f64, b: f64) -> f64 {
+    (n as f64 * b) + a / (n as f64 + 1.0).sqrt()
+}
