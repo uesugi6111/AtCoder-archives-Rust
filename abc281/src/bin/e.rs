97e5f8910d631a015b65b1820d1d2bbb90246edb
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2022-12-10 13:27:36 +0000

    abc281

---
+++ abc281/src/bin/e.rs
@@ -0,0 +1,53 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(n: usize, m: usize, k: usize, a: [i64; n]);
+
+    let mut set = std::collections::BTreeSet::new();
+    let mut pool = std::collections::BTreeSet::new();
+    for i in 0..m {
+        set.insert((a[i], -(i as i64)));
+    }
+    for _ in 0..(m - k) {
+        let buff = *set.iter().rev().next().unwrap();
+        set.remove(&buff);
+        pool.insert(buff);
+    }
+    let mut sum = set.iter().map(|x| x.0).sum::<i64>();
+    print!("{}", sum);
+
+    for i in 1..(n - m + 1) {
+        pool.insert((a[i + m - 1], -((i + m - 1) as i64)));
+        if set.remove(&(a[i - 1], -((i - 1) as i64))) {
+            let buff = *pool.iter().next().unwrap();
+            set.insert(buff);
+            pool.remove(&buff);
+            sum += buff.0;
+            sum -= a[i - 1];
+        } else {
+            pool.remove(&(a[i - 1], -((i - 1) as i64)));
+        }
+
+        let buff = *set.iter().rev().next().unwrap();
+        pool.insert(buff);
+        set.remove(&buff);
+        sum -= buff.0;
+        let buff = *pool.iter().next().unwrap();
+        set.insert(buff);
+        pool.remove(&buff);
+        sum += buff.0;
+
+        print!(" {}", sum);
+    }
+    println!();
+}
