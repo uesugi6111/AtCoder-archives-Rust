d306ec32a13870cc71e92a4feb5792b25df7cf73
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2023-01-07 15:36:49 +0000

    abc284

---
+++ abc284/src/bin/e.rs
@@ -0,0 +1,46 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::stdin().lock());input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::iter::Peekable<std::str::SplitAsciiWhitespace<'static>>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace().peekable(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace().peekable();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(n: usize, m: usize, uv: [(usize, usize); m]);
+
+    let mut e = vec![vec![]; n];
+
+    for (u, v) in uv {
+        e[u - 1].push(v - 1);
+        e[v - 1].push(u - 1);
+    }
+    let mut memo = std::collections::HashSet::new();
+    let ans = f(&mut memo, &e, 0);
+
+    println!("{}", ans);
+}
+
+fn f(memo: &mut std::collections::HashSet<usize>, e: &[Vec<usize>], n: usize) -> i64 {
+    memo.insert(n);
+
+    let mut ans = 1;
+
+    for &i in e[n].iter() {
+        if memo.contains(&i) {
+            continue;
+        }
+        if ans > 1000000 {
+            return 1000000;
+        }
+        ans += f(memo, e, i);
+    }
+
+    memo.remove(&n);
+
+    ans.min(1000000)
+}
