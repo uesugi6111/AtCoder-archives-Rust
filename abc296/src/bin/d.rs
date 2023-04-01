6d3ee037e27e767cf056ae07a4d424aa06ce73de
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2023-04-01 13:38:42 +0000

    abc296

---
+++ abc296/src/bin/d.rs
@@ -0,0 +1,33 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::BufReader::new(std::io::stdin().lock()));input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::str::SplitAsciiWhitespace<'static>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(n: i64, m: i64);
+    if (n as u128 * n as u128) < m as u128 {
+        println!("-1");
+        return;
+    }
+
+    let mut min = std::i64::MAX;
+
+    for i in 1..(n + 1).min((m as f64).sqrt() as i64 + 2) {
+        let buff = m / i;
+        if buff <= n && buff * i == m {
+            println!("{}", m);
+            return;
+        }
+        if buff < n && (buff + 1) * i >= m {
+            min = min.min((buff + 1) * i);
+        }
+    }
+    println!("{}", if min == std::i64::MAX { -1 } else { min });
+}
