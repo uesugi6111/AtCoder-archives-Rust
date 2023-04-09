2528a0c800295c8e0986da35812e5899c347565b
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2023-04-09 13:55:41 +0000

    abc297

---
+++ abc297/src/bin/c.rs
@@ -0,0 +1,31 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::BufReader::new(std::io::stdin().lock()));input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::str::SplitAsciiWhitespace<'static>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(h: usize, w: usize, mut s: [Chars; h]);
+
+    for i in 0..h {
+        for j in 1..w {
+            if s[i][j - 1] == 'T' && s[i][j] == 'T' {
+                s[i][j - 1] = 'P';
+                s[i][j] = 'C';
+            }
+        }
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            print!("{}", s[i][j]);
+        }
+        println!();
+    }
+}
