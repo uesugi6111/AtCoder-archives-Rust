dc632f1b7e1ffb6abdbb89ff0256cee7ced05c12
uesugi6111 <59960488+uesugi6111@users.noreply.github.com>
2023-12-16 23:03:52 +0900

    abc333

---
+++ abc333/src/bin/c.rs
@@ -0,0 +1,42 @@
+#[rustfmt::skip]
+mod io_pro {
+    #[macro_export] macro_rules! input{(sc=$sc:expr,$($r:tt)*)=>{input_inner!{$sc,$($r)*}};($($r:tt)*)=>{let mut sc=io_pro::Scanner::new(std::io::BufReader::new(std::io::stdin().lock()));input_inner!{sc,$($r)*}};}
+    #[macro_export] macro_rules! input_inner{($sc:expr)=>{};($sc:expr,)=>{};($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{let $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};($sc:expr,mut $var:ident:$t:tt$($r:tt)*)=>{let mut $var=read_value!($sc,$t);input_inner!{$sc $($r)*}};}
+    #[macro_export] macro_rules! read_value{($sc:expr,($($t:tt),*))=>{($(read_value!($sc,$t)),*)};($sc:expr,[$t:tt;$len:expr])=>{(0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()};($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};($sc:expr,Usize1)=>{read_value!($sc,usize)-1};($sc:expr,$t:ty)=>{$sc.next::<$t>()};}
+    pub struct Scanner{s:Box<str>,input:std::str::SplitAsciiWhitespace<'static>,}
+    impl Scanner{
+        pub fn new<R:std::io::Read>(mut reader:R)->Self{let mut sc=Scanner{s:{let mut s=String::new();reader.read_to_string(&mut s).unwrap();s.into_boxed_str()},input:"".split_ascii_whitespace(),};let s:&'static str=unsafe{std::mem::transmute(&*sc.s)};sc.input=s.split_ascii_whitespace();sc}
+        #[inline]pub fn next<T:std::str::FromStr>(&mut self)->T where T::Err:std::fmt::Debug,{self.input.next().unwrap().parse::<T>().expect("Parse error")}
+    }
+}
+#[proconio::fastout]
+fn main() {
+    input!(n: usize);
+    let mut one_set = std::collections::BTreeSet::new();
+    let mut two_set = std::collections::BTreeSet::new();
+    let mut three_set = std::collections::BTreeSet::new();
+
+    for i in 1..18 {
+        let one = "1".repeat(i).parse::<i64>().unwrap();
+        one_set.insert(one);
+        let mut v = vec![];
+        for k in &one_set {
+            v.push(k + one);
+        }
+        for k in v {
+            two_set.insert(k);
+        }
+        let mut v = vec![];
+        for k in &two_set {
+            v.push(k + one);
+        }
+        for k in v {
+            three_set.insert(k);
+        }
+
+        if three_set.len() > n {
+            break;
+        }
+    }
+    println!("{}", three_set.iter().skip(n - 1).next().unwrap());
+}
